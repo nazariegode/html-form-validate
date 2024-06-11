3938ee2ebaf74b5b6ebbe86248544a30b7dcc6d4
David Nazariego Ulacio <dnazariego@gmail.com>
2024-06-11 20:57:58 +0000

    form

--- src/app.js
+++ src/app.js
@@ -2,10 +2,90 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let form = document.getElementById("form");
+let card = document.getElementById("card");
+let cvc = document.getElementById("cvc");
+let amount = document.getElementById("amount");
+let name = document.getElementById("name");
+let lastn = document.getElementById("lastn");
+let city = document.getElementById("city");
+let states = document.getElementById("states");
+let postal = document.getElementById("postal");
+let textBox = document.getElementById("textBox");
+let alerta = document.getElementById("alert");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  if (card.value === "") {
+    card.classList.add("is-invalid");
+    alerta.classList.remove("d-none");
+  } else {
+    alerta.classList.add("d-none");
+    card.classList.remove("is-invalid");
+  }
+
+  if (cvc.value === "") {
+    cvc.classList.add("is-invalid");
+    alerta.classList.remove("d-none");
+  } else {
+    cvc.classList.remove("is-invalid");
+    alerta.classList.add("d-none");
+  }
+
+  if (amount.value === "") {
+    amount.classList.add("is-invalid");
+    alerta.classList.remove("d-none");
+  } else {
+    alerta.classList.add("d-none");
+    amount.classList.remove("is-invalid");
+  }
+
+  if (name.value === "") {
+    name.classList.add("is-invalid");
+    alerta.classList.remove("d-none");
+  } else {
+    alerta.classList.add("d-none");
+    name.classList.remove("is-invalid");
+  }
+
+  if (lastn.value === "") {
+    lastn.classList.add("is-invalid");
+    alerta.classList.remove("d-none");
+  } else {
+    lastn.classList.remove("is-invalid");
+    alerta.classList.add("d-none");
+  }
+
+  if (city.value === "") {
+    city.classList.add("is-invalid");
+    alerta.classList.remove("d-none");
+  } else {
+    alerta.classList.add("d-none");
+    city.classList.remove("is-invalid");
+  }
+
+  if (states.value === "") {
+    states.classList.add("is-invalid");
+    alerta.classList.remove("d-none");
+  } else {
+    alerta.classList.add("d-none");
+    states.classList.remove("is-invalid");
+  }
+
+  if (postal.value === "") {
+    postal.classList.add("is-invalid");
+    alerta.classList.remove("d-none");
+  } else {
+    alerta.classList.add("d-none");
+    postal.classList.remove("is-invalid");
+  }
+
+  if (textBox.value === "") {
+    textBox.classList.add("is-invalid");
+    alerta.classList.remove("d-none");
+  } else {
+    alerta.classList.add("d-none");
+    textBox.classList.remove("is-invalid");
+  }
+});
